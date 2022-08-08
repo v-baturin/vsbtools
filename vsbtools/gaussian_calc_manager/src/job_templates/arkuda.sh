43ea652bec02289e2f3867d350f322c90ddd94eb
Vladimir Baturin <vsbat@yandex.ru>
2022-08-08 19:06:15 +0200

    small cleanup

--- vsbtools/gaussian_calc_manager/src/job_templates/arkuda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#PBS -l nodes=1:ppn=10,walltime=0:01:00
-#PBS -N  Cd14Se2i1_
-#PBS -j oe
-#PBS -V
-cd ${PBS_O_WORKDIR}
-module load MPI/impi/2017.0.4 MKL/2017.4.239
-$GAUSS_EXEDIR/g16 <  Cd14Se2.gjf  > log
--- vsbtools/gaussian_calc_manager/src/job_templates/oleg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#SBATCH -o out
-#SBATCH -p lenovo
-#SBATCH -J boron
-#SBATCH -t 00:00:02
-#SBATCH -N 1
-#SBATCH -n 8
-$GAUSS_EXEDIR/g16 <  B_17_3.gjf  > log
