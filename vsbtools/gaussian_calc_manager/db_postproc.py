4e3920c22516bdb15b168eeccd82080a8490bf98
Vladimir Baturin <vsbat@yandex.ru>
2022-08-24 04:01:26 +0200

    Substantially reworked db_postproc, Now the master dictionary has already the sorting built in. The summary table is more flexible and can include user-requested columns

--- vsbtools/gaussian_calc_manager/db_postproc.py
+++ vsbtools/gaussian_calc_manager/db_postproc.py
@@ -1,5 +1,7 @@
 #!python3
 import os
+import warnings
+from typing import Union, Callable, Any, List
 import pickle
 import numpy as np
 from tools_stability.aux_routines import list_fmt2table, table2list_fmt
@@ -8,7 +10,7 @@ from ase import Atoms
 from pathlib import Path
 from cclib.parser.utils import PeriodicTable as pt
 from genutils.abInitio_io_parse.gau_parse import get_gap, get_formula, get_kohn_sham
-from genutils.misc import rgetattr
+from genutils.misc import rhasattr, rgetattr
 from matplotlib import pyplot as plt
 from graph_utils.my_graphs import draw_spectrum
 from graph_utils.formatting import cm2inch, set_ax_position_cm
@@ -47,19 +49,18 @@ def process_db_file_list(file_list,
     if element_nos and not element_symbols:
         element_symbols = tuple(element_labels[i] for i in element_nos)
 
-    master_dict = db_path_to_dict(file_list,
-                                  element_nos=element_nos,
-                                  element_symbols=element_symbols)
+    master_dict = db_file_list_to_dict(file_list,
+                                       element_nos=element_nos,
+                                       element_symbols=element_symbols)
     if write_poscars:
         write_db_to_poscars(master_dict,
                             str(res_folder) + '/POSCARS')
     if write_xyz:
-        write_xyz_of_n_lowest(master_dict, n_lowest=n_isoms, outdir=str(res_folder) + '/xyz_files',
-                              first_connected_map=first_connected_map, sortby=sortby)
+        write_xyz_of_n_lowest(master_dict, n_lowest=n_isoms, outdir=str(res_folder) + '/xyz_files')
     if write_text:
-        best_list = sorted_bests(master_dict, first_connected_map=first_connected_map, lowest_inds_dict_out=False,
-                                 sortby=sortby)
-        write_txt_data(best_list, element_symbols=element_symbols, res_folder=str(res_folder), sortby=sortby)
+        # best_list = sorted_bests(master_dict, first_connected_map=first_connected_map, lowest_inds_dict_out=False,
+        #                          sortby=sortby)
+        write_txt_data(master_dict, element_symbols=element_symbols, res_folder=str(res_folder))
 
     return master_dict
 
@@ -72,17 +73,19 @@ def get_task_db(file_list):
             loaded_tasks = pickle.load(pklfid)
             for tsk in loaded_tasks:
                 tsk.db_file = str(pklfile)
+                tsk.fold_ind = int(tsk.name.split('_')[-1])
             task_db += loaded_tasks
 
     return task_db
 
 
 
-def db_path_to_dict(file_list,
-                    sort_by = 'scfenergies', last_value = True,
-                    element_nos=None, element_symbols=None,
-                    res_folder=None,
-                    skipnangap=False):
+def db_file_list_to_dict(file_list,
+                         sort_by: Union[str, Callable] = 'ccdata.scfenergies', last_value = True,
+                         element_nos=None, element_symbols=None,
+                         res_folder=None,
+                         first_connected_map=None,
+                         skipnangap=False):
 
     """
     Utility analysing the folder containing database pkl files (recursively) and returning
@@ -112,22 +115,45 @@ def db_path_to_dict(file_list,
         except AttributeError:
             print(task.name + ' in ' + task.db_file + ' : job failed')
             continue
-        gap = get_gap(task.ccdata)
-        if (skipnangap or np.isfinite(gap)):
-            if comp in master_dict:
-                master_dict[comp]['old_ind'].append(int(task.name.split('_')[-1]))
-                master_dict[comp]['energies'].append(task.ccdata.scfenergies[-1])
-                master_dict[comp]['ccdata'].append(task.ccdata)
-                master_dict[comp]['taskname'].append(task.name)
-                master_dict[comp]['where'].append(task.db_file.split('/')[-1])
-                master_dict[comp]['gap'].append(gap)
-            else:
-                master_dict[comp] = {'old_ind': [int(task.name.split('_')[-1])],
-                                     'energies': [task.ccdata.scfenergies[-1]],
-                                     'ccdata': [task.ccdata],
-                                     'taskname': [task.name],
-                                     'where': [task.db_file.split('/')[-1]],
-                                     'gap':[gap]}
+        if hasattr(sort_by, '__call__'):
+            sort_val = sort_by(task.ccdata)
+        elif rhasattr(task, sort_by):
+            sort_val = rgetattr(task, sort_by)
+        else:
+            warnings.warn("Task " + task.name + " has no sorting value: " + str(sort_by))
+            continue
+        if last_value:
+            sort_val = sort_val[-1]
+        if comp in master_dict:
+            master_dict[comp]['tasks'].append(task)
+            master_dict[comp]['sortvalues'].append(sort_val)
+            master_dict[comp]['fold_ind'].append(int(task.name.split('_')[-1]))
+            # master_dict[comp]['energies'].append(task.ccdata.scfenergies[-1])
+            # master_dict[comp]['ccdata'].append(task.ccdata)
+            # master_dict[comp]['taskname'].append(task.name)
+            # master_dict[comp]['where'].append(task.db_file.split('/')[-1])
+            # master_dict[comp]['gap'].append(gap)
+        else:
+            master_dict[comp] = {  'tasks': [task],
+                                   'sortvalues': [sort_val],
+                                   'fold_ind': [int(task.name.split('_')[-1])],
+            #                      'energies': [task.ccdata.scfenergies[-1]],
+            #                      'ccdata': [task.ccdata],
+            #                      'taskname': [task.name],
+            #                      'where': [task.db_file.split('/')[-1]],
+                                 }
+    for cmp, val in master_dict.items():
+        idx = np.argsort(val['sortvalues'])
+        if first_connected_map is not None:
+            min_old_idx = first_connected_map[cmp]
+        else:
+            min_old_idx = 0
+        if sort_by == 'ccdata.scfenergies' or sort_by == 'ccdata.freeenergy':
+            val[sort_by] = [val['sortvalues'][int(i)] for i in idx if val['fold_ind'][int(i)] >= min_old_idx]
+        val['tasks'] = [val['tasks'][int(i)] for i in idx if val['fold_ind'][int(i)] >= min_old_idx]
+        val['fold_ind'] = [val['fold_ind'][int(i)] for i in idx if val['fold_ind'][int(i)] >= min_old_idx]
+        del val['sortvalues']
+
     return master_dict
 
 def write_db_to_poscars(master_dict, poscar_fname, vacuumsize=15., append=True):
@@ -144,72 +170,95 @@ def write_db_to_poscars(master_dict, poscar_fname, vacuumsize=15., append=True):
                   vasp5=True)
 
 
-def write_xyz_of_n_lowest(master_dict, n_lowest, outdir='xyz_files', first_connected_map=None, sortby='energies'):
+def write_xyz_of_n_lowest(master_dict, n_lowest, outdir='xyz_files'):
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    best_list, best_idcs = sorted_bests(master_dict, first_connected_map=first_connected_map, n_lowest=n_lowest,
-                                        lowest_inds_dict_out=True, sortby=sortby)
     for cmp, val in master_dict.items():
-        for i, lowest_k in enumerate(best_idcs[cmp]):
-            val['ccdata'][lowest_k].metadata['comments'] = [get_formula(val['ccdata'][lowest_k]) +
-                                                            ' d' + sortby + ' = {:6.5f}'.format(val[sortby][lowest_k] -
-                                                                                  val[sortby][best_idcs[cmp][0]])]
-            val['ccdata'][lowest_k].writexyz(outdir + '/' + val['taskname'][lowest_k] + '_g' +
+        n_avail = min(n_lowest, len(val['tasks']))
+        for i in range(n_avail):
+            print(cmp, i)
+            val['tasks'][i].ccdata.metadata['comments'] = [get_formula(val['tasks'][i].ccdata) +
+                                                     ' dE = {:6.5f}'.format(val['tasks'][i].ccdata.scfenergies[-1] -
+                                                                                val['tasks'][0].ccdata.scfenergies[-1])]
+            val['tasks'][i].ccdata.writexyz(outdir + '/' + get_formula(val['tasks'][i].ccdata) + '_' + str(val['fold_ind'][i]) + '_g' +
                                              str(i) + '.xyz')
 
 
-def sorted_bests(master_dict, first_connected_map=None, n_lowest=1, lowest_inds_dict_out=True, sortby='energies'):
-    """
-    @param first_connected_map: BINARY SYSTEMS ONLY! (SiH, PdBi etc.)  2d array of numbers of lowest fully-connected
-                                 isomers
-    @return: :rtype: (dict, list)
-    Return
-    list of information of lowest energy structures for each composition.
-    Each element of the list is of the following format:
-    [[composition], energy, gap, is_switched]
-    Example for C6H12, which has lowest structure different from the one obtained from :
-    [[6, 1], -2255.445, 3.5, True]
-    """
+# def sorted_bests(master_dict, first_connected_map=None, n_lowest=1, lowest_inds_dict_out=True, sortby='energies'):
+#     """
+#     @param first_connected_map: BINARY SYSTEMS ONLY! (SiH, PdBi etc.)  2d array of numbers of lowest fully-connected
+#                                  isomers
+#     @return: :rtype: (dict, list)
+#     Return
+#     list of information of lowest energy structures for each composition.
+#     Each element of the list is of the following format:
+#     [[composition], energy, gap, is_switched]
+#     Example for C6H12, which has lowest structure different from the one obtained from :
+#     [[6, 1], -2255.445, 3.5, True]
+#     """
+#
+#     list_fmt_best = []
+#     n_lowest_dict = {}
+#
+#     sorted_cmp = get_sorted_compositions(master_dict)
+#
+#     for cmp in sorted_cmp:
+#         comp = tuple(cmp)
+#         val = master_dict[comp]
+#         if first_connected_map is not None:
+#             lowest_connected = int(first_connected_map[comp])
+#         else:
+#             lowest_connected = 0
+#         new_ind = np.argsort(val[sortby])
+#         n_lowest_inds = new_ind[np.arange(lowest_connected, min(lowest_connected + n_lowest, len(new_ind)))]
+#         assert len(n_lowest_inds) > 0, "Invalid first_connected_map"
+#         lowest_val = val[sortby][n_lowest_inds[0]]
+#         changed = (n_lowest_inds[0] != 0)
+#         gap = val['gap'][n_lowest_inds[0]]
+#         list_fmt_best.append([list(comp)] + [lowest_val] + [gap] + [changed] + [n_lowest_inds[0]])
+#         if lowest_inds_dict_out:
+#             n_lowest_dict[comp] = n_lowest_inds
+#     if lowest_inds_dict_out:
+#         return list_fmt_best, n_lowest_dict
+#     else:
+#         return list_fmt_best
 
-    list_fmt_best = []
-    n_lowest_dict = {}
 
-    sorted_cmp = get_sorted_compositions(master_dict)
-
-    for cmp in sorted_cmp:
-        comp = tuple(cmp)
-        val = master_dict[comp]
-        if first_connected_map is not None:
-            lowest_connected = int(first_connected_map[comp])
+def write_txt_data(master_dict, element_symbols, res_folder, attributes: Any =['ccdata.scfenergies', 'fold_ind', 'db_file'], n_isom=1):
+    n_el = len(list(master_dict.keys())[0])
+    if attributes is 'energies':
+        attributes = ['ccdata.scfenergies']
+    # if n_el == 2:
+    #     flatten_list = [x[0] + x[1:-1] for x in list_fmt_best]
+    #     list_fmt2table(np.array(flatten_list)[:, [0, 1, 2]], outfile=res_folder + '/en_table.txt')
+    #     list_fmt2table(np.array(flatten_list)[:, [0, 1, 3]], outfile=res_folder + '/gap_table.txt')
+    # with open(res_folder + '/n_m_Enm_gap_2.txt', 'w') as stats_fid, open(res_folder + '/all_gaps.txt', 'w') as gaps_fid:
+    #     stats_fid.write(('%s\t' * n_el) % element_symbols + ' ' + sortby + '\tGap, ev\tinitial ind\n')
+    #     gaps_fid.write(('%s\t' * n_el) % element_symbols + '\tGap, ev\n')
+    #     for dt in list_fmt_best:
+    #         stats_fid.write(('%d\t' * n_el + '%.6f\t%.6f\t%s\t%d\n') % tuple(dt[0] + dt[1:]))
+    #         gaps_fid.write(('%d\t' * n_el + '%.6f\n') % (tuple(dt[0]) + (dt[2],)))
+    str_attr = []
+    for attr in attributes:
+        if hasattr(attr, '__call__'):
+            str_attr.append(attr.__name__)
         else:
-            lowest_connected = 0
-        new_ind = np.argsort(val[sortby])
-        n_lowest_inds = new_ind[np.arange(lowest_connected, min(lowest_connected + n_lowest, len(new_ind)))]
-        assert len(n_lowest_inds) > 0, "Invalid first_connected_map"
-        lowest_val = val[sortby][n_lowest_inds[0]]
-        changed = (n_lowest_inds[0] != 0)
-        gap = val['gap'][n_lowest_inds[0]]
-        list_fmt_best.append([list(comp)] + [lowest_val] + [gap] + [changed] + [n_lowest_inds[0]])
-        if lowest_inds_dict_out:
-            n_lowest_dict[comp] = n_lowest_inds
-    if lowest_inds_dict_out:
-        return list_fmt_best, n_lowest_dict
-    else:
-        return list_fmt_best
-
-
-def write_txt_data(list_fmt_best, element_symbols, res_folder, sortby='energies'):
-    n_el = len(list_fmt_best[0][0])
-    if n_el == 2:
-        flatten_list = [x[0] + x[1:-1] for x in list_fmt_best]
-        list_fmt2table(np.array(flatten_list)[:, [0, 1, 2]], outfile=res_folder + '/en_table.txt')
-        list_fmt2table(np.array(flatten_list)[:, [0, 1, 3]], outfile=res_folder + '/gap_table.txt')
-    with open(res_folder + '/n_m_Enm_gap_2.txt', 'w') as stats_fid, open(res_folder + '/all_gaps.txt', 'w') as gaps_fid:
-        stats_fid.write(('%s\t' * n_el) % element_symbols + ' ' + sortby + '\tGap, ev\tinitial ind\n')
-        gaps_fid.write(('%s\t' * n_el) % element_symbols + '\tGap, ev\n')
-        for dt in list_fmt_best:
-            stats_fid.write(('%d\t' * n_el + '%.6f\t%.6f\t%s\t%d\n') % tuple(dt[0] + dt[1:]))
-            gaps_fid.write(('%d\t' * n_el + '%.6f\n') % (tuple(dt[0]) + (dt[2],)))
+            str_attr.append(attr.split('.')[-1])
+    sorted_comps = get_sorted_compositions(master_dict)
+    with open(res_folder + '/n_m_Enm_gap_2.txt', 'w') as stats_fid:
+        stats_fid.write(('%s\t' * (n_el + len(attributes)) ) % (element_symbols +  tuple(attributes)) + '\n')
+        for comp in sorted_comps:
+            cmp = tuple(comp)
+            for i in range(np.min((n_isom, len(master_dict[cmp])))):
+                res_str = ('%d\t' * n_el) % cmp
+                for attr in attributes:
+                    if hasattr(attr, '__call__'):
+                        res_str += ('\t' + str(attr(master_dict[cmp]['tasks'][i].ccdata)))
+                    elif attr == 'ccdata.scfenergies':
+                        res_str += ('\t' + str(rgetattr(master_dict[cmp]['tasks'][i], attr)[-1]))
+                    else:
+                        res_str += ('\t' + str(rgetattr(master_dict[cmp]['tasks'][i], attr)))
+                stats_fid.write(res_str + '\n')
 
 
 def get_sorted_compositions(master_dict):
