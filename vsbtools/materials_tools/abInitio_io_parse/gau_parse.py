9a1cbcd976475275ff5ea0fea0d2cc9adf82c1e6
Vladimir Baturin <vsbat@yandex.ru>
2022-10-19 18:02:32 +0200

    remove redundant methods, cleaner code, decoupled atomic contributions and ipr

--- vsbtools/materials_tools/abInitio_io_parse/gau_parse.py
+++ vsbtools/materials_tools/abInitio_io_parse/gau_parse.py
@@ -12,6 +12,7 @@ from functools import wraps
 ptable = pt()
 element_labels = np.array(ptable.element[:])
 
+
 def path_or_ccobject(f):
     @wraps(f)
     def wrapped(*args, **kwargs):
@@ -21,6 +22,7 @@ def path_or_ccobject(f):
         return f(*args, **kwargs)
     return wrapped
 
+
 @path_or_ccobject
 def trace_relaxation(gaudata, outfolder, fname_root=None):
     os.makedirs(outfolder, exist_ok=True)
@@ -28,17 +30,18 @@ def trace_relaxation(gaudata, outfolder, fname_root=None):
     for k in range(len(gaudata.atomcoords)):
         gaudata.metadata['comments'].append('E_tot = {:6.5f}'.format(gaudata.scfenergies[k]))
         gaudata.writexyz(add_index(outfolder + '/' + fname_root.split('/')[-1].split('.')[0] + '.xyz', zerobased=True,
-                                       respect_file_extension=True), indices=k)
+                                   respect_file_extension=True), indices=k)
+
 
 @path_or_ccobject
 def get_kohn_sham(gaudata):
-
     homo_idx = gaudata.homos[0]
     up_states = gaudata.moenergies[0]
     dn_states = gaudata.moenergies[-1]
     fermi = 0.5 * up_states[homo_idx] + 0.5 * up_states[homo_idx + 1]
 
     return up_states, dn_states, fermi
+
 
 @path_or_ccobject
 def get_formula(gaudata, extended_out=False):
@@ -68,29 +71,53 @@ def get_formula(gaudata, extended_out=False):
     else:
         return formula
 
+
 @path_or_ccobject
 def get_orbital_coefficients(gaudata):
     coeffs = gaudata.mocoeffs[0]
     basis_ranges = get_atombasis(gaudata)
     return coeffs, basis_ranges
 
+
 @path_or_ccobject
 def get_atombasis(gaudata):
     # [{'label': {'Cd'}, 'range': [0, 24]},
     #  {'label': {'Cd'}, 'range': [24, 48]},...]
-    rgs = []
+    ranges = []
     # basisbyatom = np.array(gaudata.atombasis)
     basisbyatom = gaudata.atombasis
     symbols_list = list(element_labels[gaudata.atomnos])
     for idx, rangeslist in enumerate(basisbyatom):
-        rgs.append({'label': {symbols_list[idx]}, 'range': [rangeslist[0], rangeslist[-1] + 1]})
-    return rgs
+        ranges.append({'label': {symbols_list[idx]}, 'range': [rangeslist[0], rangeslist[-1] + 1]})
+    return ranges
+
 
 @path_or_ccobject
 def get_mullik_contributions(gaudata):
+    """
+    aoresults[s][MO][AO] = c(MO, AO) * \sum_{AO'}{c(MO, AO') * <AO|AO'>} -- Mullilken contribution of atomic orbital AO
+    to molecular orbital MO. So that sum_{AO} {aoresults[s][MO][AO]} = <MO|MO> = 1.
+    Here c(MO, AO) is axpansion coefficient: |MO>= \sum_{AO}{c(MO, AO) * |AO>}
+    :param gaudata: result of parsing of gaussian file
+    :return: aouresults[spin_component][MO][AO]
+    """
     m = MPA(gaudata)
     m.calculate()
     return np.array(m.aoresults)
+
+@path_or_ccobject
+def get_atomic_contribs(gaudata):
+    # Returns atomic contributions to orbitals (Mulliken charges)
+    mulliken = get_mullik_contributions(gaudata)
+    rgs = get_atombasis(gaudata)
+    n_MO = parseddata.nmo
+    n_atom = parseddata.natom
+    atomic_contribs = np.zeros((n_MO, natom))
+    for k_atom in range(natom):
+        orbs_of_k_atom = range(rgs[k_atom]['range'][0], rgs[k_atom]['range'][1])
+        for i_orbital in range(n_MO):
+            atomic_contribs[i_orbital, k_atom] = np.sum(mulliken[0][i_orbital][orbs_of_k_atom])
+    return atomic_contribs
 
 
 def get_pdos_on_atoms(log_filename, atomic_numbers, method='mulliken'):
@@ -99,32 +126,19 @@ def get_pdos_on_atoms(log_filename, atomic_numbers, method='mulliken'):
     print("Attention! Atomic numbers will be interpreted as 1-based")
     atomic_numbers -= 1
     pdos_coeff = 0 * coef[0]
-    # if method == 'coeff': # TO CHECK VERY CAREFULLY!!!
-    #     coef /= np.sqrt(sum(coef ** 2, 0))  # Normalizing coefficients!
-    #     for at_no in atomic_numbers:
-    #         pdos_coeff += np.sum(coef[rgs[at_no]['range'][0]: rgs[at_no]['range'][1]] ** 2, 0)
-    if method == 'mulliken':
-        mulcontrib = get_mullik_contributions(log_filename)[0]
-        for at_no in atomic_numbers:
-            pdos_coeff += np.sum(mulcontrib[:, rgs[at_no]['range'][0]: rgs[at_no]['range'][1]], 1)
+    mulcontrib = get_mullik_contributions(log_filename)[0]
+    for at_no in atomic_numbers:
+        pdos_coeff += np.sum(mulcontrib[:, rgs[at_no]['range'][0]: rgs[at_no]['range'][1]], 1)
 
     return pdos_coeff
 
-# @path_or_ccobject
-# def getcontribs(parseddata):
-#     # Returns atomic contributions to orbitals (Mulliken charges)
-#
-#     mulliken = get_mullik_contributions(parseddata)
-#     rgs = get_atombasis(parseddata)
-#     norb = parseddata.nmo
-#     natom = parseddata.natom
-#     contribs = np.zeros((norb, natom))
-#     for k_atom in range(natom):
-#         orbs_of_k_atom = range(rgs[k_atom]['range'][0], rgs[k_atom]['range'][1])
-#         for i_orbital in range(norb):
-#             contribs[i_orbital, k_atom] = np.sum(mulliken[0][i_orbital][orbs_of_k_atom])
-#     ipr = np.sum(contribs ** 2, 1)  # $\sum_i{c_i^4}$
-#     return ipr, contribs, parseddata.moenergies, parseddata.homos
+
+@path_or_ccobject
+def get_ipr(parseddata):
+    # Returns inverse population ratio (IPR)
+    contribs = get_atomic_contribs(parseddata)
+    ipr = np.sum(contribs ** 2, 1)  # $\sum_i{c_i^4}$
+    return ipr
 
 
 def getpdos_general(gaudata, flags_list, method='mulliken'):
@@ -156,32 +170,22 @@ def getpdos_general(gaudata, flags_list, method='mulliken'):
                 orbrange.append(orb[0])
             elif len(andgroup) == 1 and (search_pattern in (orbnamesplit[0] + '_')):
                 orbrange.append(orb[0])
-
-    # Constructing partial DOS coefficients
-    # if method == 'coeff':
-    #     coef /= np.sqrt(sum(coef ** 2, 0))  # Normalizing coefficients!
-    #     for at_no in atomic_numbers:
-    #         pdos_coeff += np.sum(coef[rgs[at_no]['range'][0]: rgs[at_no]['range'][1]] ** 2, 0)
-    if method == 'mulliken':
-        mull_contrib = get_mullik_contributions(gaudata)
-        up_mull_contrib = mull_contrib[0]
-        pdos_coeff = np.sum(up_mull_contrib[:, orbrange], 1)
+    mull_contrib = get_mullik_contributions(gaudata)
+    up_mull_contrib = mull_contrib[0]
+    pdos_coeff = np.sum(up_mull_contrib[:, orbrange], 1)
     print(np.sum(up_mull_contrib))
     return pdos_coeff, gaudata.moenergies  # , orbrange, gaudata
 
+
 @path_or_ccobject
 def get_total_energy(parseddata):
-    # if isinstance(clusterdata, str):
-    #     clname = clusterdata.split('/')[0]
-    #     parseddata = ccread(clusterdata)
-    # else:
-    #     parseddata = clusterdata
     clname = get_formula(parseddata)
     try:
         return parseddata.scfenergies[-1]
     except Exception as e:
         print('No scf done: ' + e.__class__ + ' ' + clname)
         return None
+
 
 @path_or_ccobject
 def get_gap(clusterdata):
@@ -198,39 +202,3 @@ def get_gap(clusterdata):
         gap = clusterdata.moenergies[0][homo_indices[0] + 1] - \
               clusterdata.moenergies[0][homo_indices[0]]
     return gap
-    
-
-# def deprecated_get_kohn_sham(log_filename):
-#     # The function works for spin-nonpolarized cases only
-#     spec_raw = my_parse.getblock(log_filename, 'The electronic state', 'Condensed to atoms')
-#     spectrum = {'up_occ': [], 'up_free': [], 'dn_occ': [], 'dn_free': []}
-#     for spec_string in spec_raw:
-#         splt_spec_string = spec_string.split()
-#         evals = splt_spec_string[4:]
-#         if len(evals) > 0 and len(evals[0]) > 10: # Sometimes Gaussian glues together the evals: -343.44321-342.44321...
-#             evals_dummy = evals[0].strip().replace('-',' -').split()
-#             if len(evals) == 1:
-#                 evals = evals_dummy
-#             else:
-#                 evals = evals_dummy + evals[1:]
-#         if 'Alpha  occ.' in spec_string:
-#             spectrum['up_occ'].extend(evals)
-#         elif ' Alpha virt.' in spec_string:
-#             spectrum['up_free'].extend(evals)
-#         elif 'Beta  occ.' in spec_string:
-#             spectrum['dn_occ'].extend(evals)
-#         elif 'Beta virt.' in spec_string:
-#             spectrum['dn_free'].extend(evals)
-#
-#     for key in spectrum.keys():
-#         spectrum[key] = np.array([float(i) for i in spectrum[key]])
-#         # print(key, spectrum[key])
-#
-#     occ_states = np.append(spectrum['up_occ'], spectrum['dn_occ'])
-#     free_states = np.append(spectrum['up_free'], spectrum['dn_free'])
-#     fermi = 0.9 * np.max(occ_states) + 0.1 * np.min(free_states)
-#
-#     up_states = np.append(spectrum['up_occ'], spectrum['up_free'])
-#     dn_states = np.append(spectrum['dn_occ'], spectrum['dn_free'])
-#
-#     return up_states, dn_states, fermi
