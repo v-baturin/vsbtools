f543b9ac978a8301e0467b897752da40c78e40d8
Vladimir Baturin <vsbat@yandex.ru>
2022-11-22 17:04:32 +0100

    debug binding energy for 1-component system

--- vsbtools/materials_tools/tools_stability/binding_energy.py
+++ vsbtools/materials_tools/tools_stability/binding_energy.py
@@ -4,4 +4,4 @@ def binding_energy_1comp(n_vs_e_formatted, start=0, stop=None):
     e_1 = all_e_s[all_nats == 1]
     n_at_s = all_nats[slice(start, stop)]
     e_s = all_e_s[slice(start, stop)]
-    return (e_s - n_at_s * e_1) / n_at_s, n_at_s
+    return n_at_s, (e_s - n_at_s * e_1) / n_at_s
