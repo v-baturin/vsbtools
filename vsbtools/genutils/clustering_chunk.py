ae5b7dff9bbfff440caf8330efb5baec0db6d646
Vladimir Baturin <vsbat@yandex.ru>
2023-11-06 14:53:03 +0100

    improve structure of FP-distance based clustering

--- vsbtools/genutils/clustering_chunk.py
+++ vsbtools/genutils/clustering_chunk.py
@@ -2,27 +2,22 @@ import numpy as np
 from collections import deque
 
 
-def clustering_by_dist(fingerprints, rho, delta):
-    """
-    Clustering
-    splits set of nodes 0...N into clusters based on the distance mtrx and the threshold distance,
-    below which the nodes are considered as belonging to one 
-    @param fingerprints: list of fingerprint objects
-    @param rho: a function object calculating metrics in FP space
-    @param delta: fingerprint tolerance
-    @return: list of lists of classes
-    :Authors:
-    Vladimir Baturin <v.baturin@skoltech.ru>
-    """
-    matrix_dim = len(fingerprints)
-
+def make_dist_matrix(points, dist_fun):
+    matrix_dim = len(points)
+    dist_mat = np.zeros((matrix_dim, matrix_dim), dtype=float)
     adj_mat = np.zeros((matrix_dim, matrix_dim), dtype=bool)  # Boolean adjacency mtrx in FP-space
     for i in range(matrix_dim):
-        for j in range(matrix_dim):
-            adj_mat[i, j] = rho(fingerprints[i], fingerprints[j]) < delta
+        for j in range(i, matrix_dim):
+            print(f"i = {i}, j = {j}")
+            dist_mat[i, j] = dist_mat[j, i] = dist_fun(points[i], points[j])
+    return dist_mat
 
+def adj_matrix_from_dist(dist_matrix, delta):
+    return dist_matrix <= delta
+
+def get_clusters_from_adj_mat(adj_mat):
     clusters = []
-    rest = list(range(matrix_dim))
+    rest = list(range(len(adj_mat)))
     while rest:
         to_visit = deque([rest[0]])
         curr_cluster = []
@@ -36,3 +31,19 @@ def clustering_by_dist(fingerprints, rho, delta):
                 to_visit.extendleft(next_to_visit)
         clusters.append(curr_cluster)
     return clusters
+
+
+def clustering_by_dist(fingerprints, rho, delta):
+    """
+    Clustering
+    splits set of nodes 0...N into clusters based on the distance mtrx and the threshold distance,
+    below which the nodes are considered as belonging to one
+    @param fingerprints: list of fingerprint objects
+    @param rho: a function object calculating metrics in FP space
+    @param delta: fingerprint tolerance
+    @return: list of lists of classes
+    :Authors:
+    Vladimir Baturin <v.baturin@skoltech.ru>
+    """
+
+    return get_clusters_from_adj_mat(adj_matrix_from_dist(make_dist_matrix(fingerprints, rho), delta))
