c6a5199f0b73fccc04cff8f51e9d3b04805e3375
Vladimir Baturin <vladimir.s.baturin@gmail.com>
2025-11-21 17:56:51 +0900

    add formula constructor with custom element order

--- vsbtools/genutils/format_tools.py
+++ vsbtools/genutils/format_tools.py
@@ -1,3 +1,5 @@
+from ase.formula import Formula
+
 def stoich2formula(stoich, el_symbols):
     formula = ''
     for n_at, symbol in zip(stoich, el_symbols):
@@ -6,3 +8,15 @@ def stoich2formula(stoich, el_symbols):
         elif n_at > 1:
             formula += symbol + str(int(n_at))
     return formula
+
+
+
+def get_chemical_formula_custom_order(atoms, ordered_elements):
+    fla_dict = Formula(atoms.get_chemical_formula()).count()
+    result = ''
+    for el in ordered_elements:
+        if el in fla_dict:
+            result += el
+            cnt = fla_dict[el]
+            result += str(int(cnt)) if cnt !=1 else ''
+    return result
