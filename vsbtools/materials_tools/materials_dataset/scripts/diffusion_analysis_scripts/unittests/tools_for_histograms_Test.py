f502887109ce1f0bfec8b9d0b91741e428a95678
Vladimir Baturin <vladimir.s.baturin@gmail.com>
2025-12-19 15:39:16 +0900

    improve histogram plotting with better colors and better treatment of sets with no data

--- vsbtools/materials_tools/materials_dataset/scripts/diffusion_analysis_scripts/unittests/tools_for_histograms_Test.py
+++ vsbtools/materials_tools/materials_dataset/scripts/diffusion_analysis_scripts/unittests/tools_for_histograms_Test.py
@@ -77,7 +77,7 @@ class hist_tools_Test(unittest.TestCase):
         hdc = histo_data_collection(ds_dict, callable_name='compute_mean_coordination', callable_params={"type_A": 27,
                                                                                                          "type_B": 8},
                                     max_bincenter=10)
-        plot_multihistogram(multidata=hdc, target=4, max_bincenter=10)
+        plot_multihistogram_new(multidata=hdc, target=4, max_bincenter=10)
         print(hdc)
         plt.show()
 
@@ -119,7 +119,7 @@ class hist_tools_Test(unittest.TestCase):
         ds_dict = collect_stage_dataset_dict(dirs, "symmetrize_raw", "poll_db", add_guid_descr=True)
         hdc = histo_data_collection(ds_dict, callable_name='compute_mean_coordination',
                                     callable_params={"type_A": type_A, "type_B": type_B})
-        plot_multihistogram(multidata=hdc, target=target, max_bincenter=10)
+        plot_multihistogram_new(multidata=hdc, target=target, max_bincenter=10)
         print(hdc)
         plt.show()
 
@@ -168,3 +168,6 @@ class hist_tools_Test(unittest.TestCase):
         # target_entries = [e for e in ds_dict[ds_name] if target-half_width <= function(e) <= target + half_width]
         # print(len(target_entries))
         # plt.show()
+
+    # def test_empty_histogram(self):
+    #     system = "Cu-Si-P-Ca"
