fc7bb9a6f6ae02f3cee597ab72df82fb9dafb0f9
Vladimir Baturin <vladimir.s.baturin@gmail.com>
2026-01-30 16:13:48 +0900

    register all available models in nn_estimator_Test.py

--- vsbtools/materials_tools/materials_dataset/energy_estimation/unittests/nn_estimator_Test.py
+++ vsbtools/materials_tools/materials_dataset/energy_estimation/unittests/nn_estimator_Test.py
@@ -6,19 +6,21 @@ from ...io.structures_dataset_io import StructureDatasetIO
 
 PATH_WITH_TESTS = Path(__file__).parent
 PATH_WITH_DATASETS = PATH_WITH_TESTS / "../../unittests_datasets"
+AVAILABLE_MODELS = {'mattersim': mattersim_bridge, 'grace': grace_bridge}
 
 class NNEstimator_Test(unittest.TestCase):
 
     def setUp(self):
         self.poscars = PATH_WITH_DATASETS / "POSCARS"
         self.dataset = StructureDatasetIO(self.poscars).load_from_directory()
-        NNEstimator.register_model("mattersim", mattersim_bridge)
-        NNEstimator.register_model("grace", grace_bridge)
+        for name, bridge in AVAILABLE_MODELS.items():
+            NNEstimator.register_model(name, bridge)
         self.estimator = NNEstimator()
 
     def test_single_entry_estimation(self):
+        print(self.dataset[2])
         self.assertAlmostEqual(self.estimator.estimate_entry_energy(self.dataset[2]), -44.0794, places=3)
-        self.estimator.estimate_entry_energy(self.dataset[2], model='grace')
+        print(self.estimator.estimate_entry_energy(self.dataset[2], model='grace'))
 
     def test_batch_estimation(self):
         ds2 = self.estimator.estimate_dataset_energies(self.dataset)
