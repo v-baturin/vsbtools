0fd8817aab9848d645393e84ec4492226318fa93
Vladimir Baturin <vsbat@yandex.ru>
2026-01-30 14:25:33 +0900

    make grace_helper_single running

--- vsbtools/materials_tools/NN_energy_estimators/grace_helpers/grace_helper_single.py
+++ vsbtools/materials_tools/NN_energy_estimators/grace_helpers/grace_helper_single.py
@@ -1,10 +1,8 @@
-import torch
 import sys, io
 from ase.io import read
 from tensorpotential.calculator import grace_fm
 calc = grace_fm('GRACE-2L-OMAT')
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
 """
 Read an ASE JSON structure from stdin, print the total energy to stdout.
 """
